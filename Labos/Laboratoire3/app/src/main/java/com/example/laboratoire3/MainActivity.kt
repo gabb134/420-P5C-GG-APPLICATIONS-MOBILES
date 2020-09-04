ef86a6556a77838c633c2bbd0440a7828b248b52
gabb134 <cg.marrero@cgodin.qc.ca>
2020-09-04 17:17:55 -0400

    modig gab, labo 3 machine a sous en cours

--- Labos/Laboratoire3/app/src/main/java/com/example/laboratoire3/MainActivity.kt
+++ Labos/Laboratoire3/app/src/main/java/com/example/laboratoire3/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.laboratoire3
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //initialisation des imageviews
+      //  val constraintLayout = findViewById(R.id.imageView1) as ConstraintLayout
+        //val imageViewUn = ImageView(this)
+        //imageViewUn.setImageResource(R.drawable.androidlogo)
+       // constraintLayout.addView(imageViewUn)
+       // val constraintLayout = findViewById(R.id.imageView1) as ImageView
+      //  val imageView = ImageView(this)
+      //  imageView.setImageResource(R.drawable.androidlogo)
+     //   constraintLayout.setImageDrawable(imageView)
+
+
+
+        btnJouer.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        when (view) {
+            btnJouer -> {
+           // faire apparaitre les images aléatoirement...
+
+                val action = intent.action;
+                Toast.makeText(this, " Vous avez cliquer le button jouer qui se trouve dans: " + action, Toast.LENGTH_LONG).show();
+
+
+            }
+
+        }
+
     }
 }
